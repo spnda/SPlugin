fffeec265a0d5220dcbf088f102211d66e4f3014
pnda <43609023+ThePNDA@users.noreply.github.com>
2021-04-10 02:22:59 +0200

    Move to seperate git repo

--- BlockProt/src/main/kotlin/de/sean/blockprot/bukkit/inventories/BlockLockInventory.kt
+++
@@ -1,8 +0,0 @@
-package de.sean.blockprot.bukkit.inventories
-
-import org.bukkit.Bukkit
-
-object BlockLockInventory {
-    const val INVENTORY_NAME = "Block Lock"
-    var inventory = Bukkit.createInventory(null, 9, INVENTORY_NAME)
-}
