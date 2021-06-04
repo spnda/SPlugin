6c7d840df5ea7f7dd85e086ab056cc649fd11666
spnda <43609023+spnda@users.noreply.github.com>
2021-06-05 01:35:52 +0200

    Add: Use new ChatRenderer paper API

--- splugin-paper/src/main/kotlin/de/sean/splugin/paper/events/PaperMessageEvent.kt
+++ splugin-paper/src/main/kotlin/de/sean/splugin/paper/events/PaperMessageEvent.kt
@@ -3,6 +3,7 @@ package de.sean.splugin.paper.events
 import de.sean.splugin.bukkit.tasks.AfkPlayerManager
 import de.sean.splugin.discord.Discord
 import io.papermc.paper.event.player.AsyncChatEvent
+import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.event.ClickEvent
 import net.kyori.adventure.text.format.NamedTextColor
@@ -15,11 +16,11 @@ class PaperMessageEvent : Listener {
     @EventHandler
     fun chatEvent(event: AsyncChatEvent) {
         val player = event.player
-
-        event.composer { messageAuthor: Player, displayName: Component, message: Component ->
+        
+        event.renderer { messageAuthor, sourceDisplayName, message, _ ->
             Component.text()
                 .append(
-                    displayName
+                    sourceDisplayName
                         .color(NamedTextColor.WHITE)
                         .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/msg ${messageAuthor.name}"))
                 )
@@ -27,7 +28,6 @@ class PaperMessageEvent : Listener {
                 .append(message)
                 .build()
         }
-        event.composer().composeChat(event.player, event.player.displayName(), event.message())
 
         /* AFK: We unmark the player AFK when they write a message. */
         AfkPlayerManager.setLastActivity(player.uniqueId, System.currentTimeMillis())
